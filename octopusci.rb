520b12ab7ad4ddfc44f410b859d0aeb4019b6a62
Andrew De Ponte <cyphactor@gmail.com>
2011-09-07 13:42:46 -0700

    Got a little ways further in the resque integration and GitHub info.

--- octopusci.rb
+++ octopusci.rb
@@ -7,9 +7,21 @@ require 'sinatra'
 require 'json'
 require 'resque'
 
+PROJECTS = [
+  {
+    :name => 'temp_pusci_test'
+  }
+]
+
 class DrewSleep
   def self.perform()
     sleep 600
+    # Fetch the latest from the repo and checkout the ref
+    
+    # Run the build commands
+    
+    # Notify about the results
+    
   end
 end
 
@@ -31,37 +43,34 @@ get '/:project_name/:branch_name/manbuild' do
   Resque.push(q_name, :class => 'DrewSleep', :args => [])
 end
 
-post '/:project_name/build' do
-  proj_name = params[:project_name]
-  payload = params[:payload]
+post '/github-build' do
+  payload = JSON.parse(params[:payload])
+  
+  repository_name = payload["repository"]["name"]
+  branch_name = payload["ref"].gsub(/refs\/heads\//, '')
+    
+  q_name = "#{repository_name}-#{branch_name}"
+  
+  repository_url = payload["repository"]["url"]
+  compare_url = payload["compare"]
+  
+  pusher_name = payload["pusher"]["name"]
+  pusher_email = payload["pusher"]["email"]
+
+  puts "------------ BUILDING -------------"
+  puts "repository_name: #{repository_name}"
+  puts "branch_name: #{branch_name}"
+  puts "repository_url: #{repository_url}"
+  puts "compare_url: #{compare_url}"
+  puts "pusher_name: #{pusher_name}"
+  puts "pusher_email: #{pusher_email}"
+  puts "Payload: "
+  puts payload.inspect
   
   PROJECTS.each do |proj|
-    if (proj_name == proj[:name])
-      # Create project branch queue hash if it doesn't exist yet
-      if !PROJECT_QUEUES.has_key?(proj_name)
-        PROJECT_QUEUES[proj_name] = {}
-      end
-      
-      ref = payload["ref"]
-      
-      # Create branch specific queue if it doesn't already exist
-      if !PROJECT_QUEUES[proj_name].has_key?(ref)
-        PROJECT_QUEUES[proj_name][ref] = []
-      end
-      
+    if (proj[:name] == repository_name)
       # Append job to this branches queue
-      PROJECT_QUEUES[proj_name][ref].push(1)
-      
-      # Fetch the latest from the repo and checkout the ref
-      
-      # Run the build commands
-      
-      # Notify about the results
-      
-      puts "------------ BUILDING -------------"    
-      puts "Project Name: #{params[:project_name]}"
-      puts "Project Queue: #{PROJECT_QUEUES[proj_name].inspect}"
-      puts "Payload: #{JSON.parse(params[:payload]).inspect}"
+      Resque.push(q_name, :class => 'DrewSleep', :args => [])
       break
     end
   end
